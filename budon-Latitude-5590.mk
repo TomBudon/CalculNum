39f373083348d446de4d72f04333847eeb4d250d
Tom BUDON <tom.budon@ens.uvsq.fr>
2022-12-14 10:49:02 +0100

    Modification du header pour l'import de lapack
    + ajout du fichier de config budon-Latitude-5590.mk

---
+++ budon-Latitude-5590.mk
@@ -0,0 +1,7 @@
+#############################################
+# Default options for budon-latitude computer
+#############################################
+CC=gcc
+LIBSLOCAL=-L/usr/lib/x86_64-linux-gnu -llapack -lblas -lm
+INCLUDEBLASLOCAL=-I/usr/include/x86_64-linux-gnu/
+OPTCLOCAL=-fPIC -march=native
